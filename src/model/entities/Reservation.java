785429c438dad073492d5c9df1969a698f6e0a72
Kauan Masseu <masseukauan7@gmail.com>
2025-11-18 10:20:06 -0300

    versão ruim

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,5 +1,6 @@
 package model.entities;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -44,9 +45,17 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkIn, Date checkOut) {
+    public String updateDates(Date checkIn, Date checkOut) throws ParseException {
+        Date now = sdf.parse("06/06/2018");
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Reservetion dates for update must be future dates";
+        }
+        if (!checkOut.after(checkIn)) {
+           return  "Error in reservation: Check-out date must be after check-in date";
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null;
     }
 
     @Override
